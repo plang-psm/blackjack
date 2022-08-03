cfa671cd6164fb5a89ede46c46f06d2697c3c748
Phillip Sanchez <101952500+plang-psm@users.noreply.github.com>
2022-08-02 19:47:25 -0700

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,69 @@
+let cards = [];
+let sum = 0;
+let hasBlackjack = false;
+let isAlive = false;
+let message = '';
+
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
+let hitEl = document.getElementById('hit-btn');
+let startEl = document.getElementById('start-btn');
+let playerEl = document.getElementById('player');
+
+let player = {
+  name: 'Phillip',
+  chips: '250',
+};
+
+playerEl.textContent = player.name + ':' + ' $' + player.chips;
+
+function start() {
+  isAlive = true;
+  let firstCard = randomCard();
+  let secondCard = randomCard();
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
+  startEl.textContent = 'START GAME';
+  hitEl.style.backgroundColor = 'gold';
+  renderGame();
+}
+
+function renderGame() {
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?';
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+    startEl.textContent = 'PLAY AGAIN';
+    startEl.style.backgroundColor = 'gold';
+    hitEl.style.backgroundColor = 'red';
+  }
+  console.log(message);
+  messageEl.textContent = message;
+  sumEl.textContent = 'Sum: ' + sum;
+  cardsEl.textContent = 'Cards: ' + cards.join(' ');
+}
+
+function randomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
+
+function hit() {
+  if (isAlive === true && hasBlackjack === false) {
+    let newCard = randomCard();
+    sum = sum + newCard;
+    cards.push(newCard);
+    renderGame();
+  }
+}
